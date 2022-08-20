c927419b1856f8aeaccfbe76753b88ceebfb8208
IdhamSdmc <isacd.dannmc@gmail.com>
2022-08-20 16:42:26 -0500

    Cpanel config for github 03

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -218,7 +218,7 @@
                                                 </g>
                         </svg>
                 </div>
-
+                <h3>Cambio de prueba</h3>
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
