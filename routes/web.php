c125a15b9d68b363b5ae614ef57ff5a02eecbafe
IdhamSdmc <isacd.dannmc@gmail.com>
2022-08-24 15:53:14 -0500

    Libreria 2

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
