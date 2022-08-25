894a5d2a8bca390c098bfcf194045441d26dcd36
IdhamSdmc <isacd.dannmc@gmail.com>
2022-08-25 16:47:32 -0500

    Lineas

--- resources/views/pages/nosotros.blade.php
+++ resources/views/pages/nosotros.blade.php
@@ -121,73 +121,39 @@
     <section class="nuestra-historia">
         <h2>NUESTRA HISTORIA</h2>
         <div class="container">
-            <section class="timeline">
-                <ol>
-                    <li>
-                        <div>
-                            <time>1934</time> At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <time>1937</time> Proin quam velit, efficitur vel neque vitae, rhoncus commodo mi. Suspendisse finibus mauris et bibendum molestie. Aenean ex augue, varius et pulvinar in, pretium non nisi.
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <time>1940</time> Proin iaculis, nibh eget efficitur varius, libero tellus porta dolor, at pulvinar tortor ex eget ligula. Integer eu dapibus arcu, sit amet sollicitudin eros.
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <time>1943</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <time>1946</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <time>1956</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <time>1957</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <time>1967</time> Aenean condimentum odio a bibendum rhoncus. Ut mauris felis, volutpat eget porta faucibus, euismod quis ante.
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <time>1977</time> Vestibulum porttitor lorem sed pharetra dignissim. Nulla maximus, dui a tristique iaculis, quam dolor convallis enim, non dignissim ligula ipsum a turpis.
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <time>1985</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <time>2000</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <time>2005</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
-                        </div>
-                    </li>
-                    <li></li>
-                </ol>
+            <ul class="timeline">
+                <li>
+                    <div>
+                        <time datetime="2018-10-09">1996</time>
+                        <p>Comienza nuestra historia como
+                            GRUPO PALERMO dedicada al
+                            servicio de fotocopiado e
+                            impresión.</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <time datetime="2018-10-09">2010</time>
+                        <p>Alianza estratégica y de
+                            distribución exclusiva en el Perú
+                            con la marca de impresoras
+                            RICOH</p>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <time datetime="2018-10-09">2016</time>
+                        <p>Grupo Palermo se expande en el
+                            norte del país, con sucursales en
+                            Chiclayo, Piura,
+                            consolidándose como líder en los
+                            servicios de outsourcing de
+                            impresoras en el norte del país.</p>
+                    </div>
+                </li>
 
+            </ul>
 
-            </section>
         </div>
     </section>
     <script src="">
