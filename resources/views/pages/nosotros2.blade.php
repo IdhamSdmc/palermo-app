4e390142340788fe1445b72d664d66bec4df83d2
Waldir Sanchez <waldir.sanchez.p@gmail.com>
2022-08-30 16:50:23 -0500

    linea de tiempo

--- resources/views/pages/nosotros2.blade.php
+++ resources/views/pages/nosotros2.blade.php
@@ -151,14 +151,14 @@
                                     </li>
                                 </ul>
                             </div>
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25"
-                                    aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
                         </div>
                         <button type="button" class="btn-slider">
                             <img src="{{ asset('assets/imgs/right-slider.png') }}" alt="">
                         </button>
+                    </div>
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25"
+                            aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
                 </div>
             </div>
