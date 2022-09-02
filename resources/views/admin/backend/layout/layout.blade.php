c911e86e065286765840b43dc821d1933030b5f8
jairoederson <jairo.ederson@gmail.com>
2022-09-01 23:59:31 -0500

    layout update

--- resources/views/admin/backend/layout/layout.blade.php
+++ resources/views/admin/backend/layout/layout.blade.php
@@ -87,7 +87,7 @@
                                 <!-- inner menu: contains the actual data -->
 
                             </li>
-                            <li class="footer"><a href="{{ url(getLang() . '/admin/form-post') }}">See All Messages</a></li>
+                            <li class="footer"><a href="{{ url('/admin/form-post') }}">See All Messages</a></li>
                         </ul>
                     </li>
 
@@ -121,7 +121,7 @@
                             <!-- Menu Footer-->
                             <li class="user-footer">
                                 <div class="pull-left">
-                                    <a href="{{ url(getLang() . '/admin/user/' . Sentinel::getUser()->id) }}" class="btn btn-default btn-flat">Profile</a>
+                                    <a href="{{ url('/admin/user/' . Sentinel::getUser()->id) }}" class="btn btn-default btn-flat">Profile</a>
                                 </div>
                                 <div class="pull-right">
                                     <a href="{{ url('/admin/logout') }}" class="btn btn-default btn-flat">Sign out</a></div>
