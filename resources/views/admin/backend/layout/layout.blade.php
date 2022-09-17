59ed85c4dab2060e3560744f94a4f0c9a8b13ad7
jairoederson <jairo.ederson@gmail.com>
2022-09-17 17:32:13 -0500

    admin

--- resources/views/admin/backend/layout/layout.blade.php
+++ resources/views/admin/backend/layout/layout.blade.php
@@ -59,9 +59,9 @@
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// --><!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script><![endif]-->
+    @FilemanagerScript
 </head>
-<script src="{!! url('filemanager/bundle/filemanager.min.js') !!}" type="text/javascript"></script>
-<script >filemanager.baseUrl = '{{route('filemanager.base_route')}}';</script>
+
 <body class="skin-blue">
 <div class="wrapper">
     <header class="main-header">
