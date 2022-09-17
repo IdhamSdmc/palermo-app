dbf6c7509a7d9c8e99798fbaef2cbe2084d54384
jairoederson <jairo.ederson@gmail.com>
2022-09-17 16:40:18 -0500

    cambio admin

--- resources/views/admin/backend/layout/layout.blade.php
+++ resources/views/admin/backend/layout/layout.blade.php
@@ -60,8 +60,9 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script><![endif]-->
 </head>
+admin/filemanager
 <script src="{!! url('filemanager/bundle/filemanager.min.js') !!}" type="text/javascript"></script>
-<script > filemanager.baseUrl = 'http://127.0.0.1:8000/admin/filemanager';</script>
+<script > filemanager.baseUrl = "{!! url('admin/filemanager') !!}";</script>
 <body class="skin-blue">
 <div class="wrapper">
     <header class="main-header">
