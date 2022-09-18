fb9b420bdb4d720af6e031a4e70fc520f75f2b37
jairoederson <jairo.ederson@gmail.com>
2022-09-17 19:48:21 -0500

    cambio layout

--- resources/views/admin/backend/layout/layout.blade.php
+++ resources/views/admin/backend/layout/layout.blade.php
@@ -59,7 +59,8 @@
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// --><!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script><![endif]-->
-    @FilemanagerScript
+<script src="{{asset('filemanager/bundle/filemanager.min.js')}}"></script>"
+<script> filemanager.baseUrl = '{{route('filemanager.base_route')}}';</script>
 </head>
 
 <body class="skin-blue">
