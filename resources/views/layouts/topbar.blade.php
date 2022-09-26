b4dede0ed977af2cad0d601aafc2c3e2f7cc71ce
jairoederson <jairo.ederson@gmail.com>
2022-09-26 15:39:31 -0500

    prueba 3

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -28,14 +28,26 @@
     </nav>
 </header> --}}
 
-<header class="header">
-    <a href="" class="logo">CSS Nav</a>
+<header class="header-menu">
+    <a href="{{ url('/') }}" class="logo">
+        <img src="{{ url($settings['path_logo'].$settings['file_name_logo']) }}" alt="palermo-logo">
+    </a>
     <input class="menu-btn" type="checkbox" id="menu-btn" />
     <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
     <ul class="menu">
-      <li><a href="#work">Our Work</a></li>
-      <li><a href="#about">About</a></li>
-      <li><a href="#careers">Careers</a></li>
-      <li><a href="#contact">Contact</a></li>
+        <li class="topbar-item"><a href="{{ url('/nosotros') }}" class="active">Nosotros</a></li>
+        <li class="topbar-item"><a href="{{ url('/soluciones') }}">Soluciones</a></li>
+        <li class="topbar-item"><a href="{{ url('/productos') }}">Productos</a></li>
+        <li class="topbar-item"><a href="{{ url('/articulos') }}">Blog</a></li>
+
+        <li class="topbar-item">
+            <a href="{{ url('/contactanos') }}" class="btn-topbar">Contáctanos</a>
+        </li>
+        <li class="topbar-item m-support"><a href="{{ url('https://soporte.grupopalermo.pe') }}" target="_blank">Soporte Técnico</a></li>
+        <li class="topbar-item w-support">
+            <a target="_blank" href="{{ url('https://soporte.grupopalermo.pe') }}" class="btn-soporte">
+                <img src="{{ asset('assets/imgs/soporte técnico.svg') }}" alt="">
+            </a>
+        </li>
     </ul>
   </header>
